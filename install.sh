5112457a0c29367d854479922fe0bfc5380f2d1a
k-vernooy <kai.a.vernooy@gmail.com>
2020-12-17 13:08:46 -0500

    Fix symlink problems for directories

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 # link my config directory
 for i in $(ls ${dir}/config); do
-    ln -s ${dir}/config/${i} ${HOME}/.config/${i}
+    ln -s ${dir}/config/${i} ${HOME}/.config/
 done
 
 # link my home directory
@@ -21,6 +21,6 @@ for i in $(ls ${dir}/home); do
     ln -s ${dir}/home/${i} ${HOME}/.${i}
 done
 
-ln -s ${dir}/scripts/ ${HOME}/scripts/
+ln -s ${dir}/scripts/ ${HOME}/
 
 echo "You're on your own for installing the nonstandard stuff in /apps/, individual apps in that dir may have instructions if I've gotten around to it..."
