ca74a2a66df965e6d81b5c00e2acf8101c9bf777
k-vernooy <kai.a.vernooy@gmail.com>
2021-01-10 16:06:22 -0500

    Add lockfile to fix flickering

--- scripts/misc/backlight.sh
+++ scripts/misc/backlight.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-subinc=5
-subchange=$(echo "1 / $subinc" | bc -l)
-delay=$(echo "(0.02 / $2)/$subinc" | bc -l)
-opt=""
 
+lock=~/scripts/misc/backlight-lockfile
+
+while [ -f "$lock" ]; do
+    echo "process changing backlight already exists" > /dev/null
+done
+
+touch "$lock"
+
+
+subinc=2
+opt=""
+subchange=$(echo "1/$subinc" | bc -l)
 
 if [ "$1" == "inc" ]; then
     opt="-A"
@@ -12,10 +20,12 @@ else
     opt="-U"
 fi
 
-
 for i in $(seq $2); do
     for i in $(seq $subinc); do
         sudo light $opt "$subchange"
-        sleep "$delay"
     done
 done
+
+
+rm "$lock"
+
