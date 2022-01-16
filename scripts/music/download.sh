1c5068ad7e2ff0e3ceb2ab3aa5b1a7a3d022baed
k-vernooy <kai.a.vernooy@gmail.com>
2022-01-16 14:53:25 -0500

    Add image cropping and rounding to download script

--- scripts/music/download.sh
+++ scripts/music/download.sh
@@ -2,10 +2,22 @@
 
 SONG=$1
 MUSIC="$HOME/music"
-DM=1
-DT=1
 
-. $HOME/dev/venv/bin/activate
+
+if [ -z "$2" ]; then
+    DM=1
+else
+    DM="$2"
+fi
+
+if [ -z "$3" ]; then
+    DT=1
+else
+    DT="$3"
+fi
+
+
+. $HOME/venv/bin/activate
 
 
 if [ "$DM" -eq 1 ]; then
@@ -14,11 +26,37 @@ fi
 if [ "$DT" -eq 1 ]; then
     youtube-dl --write-thumbnail --skip-download -o "$MUSIC/thumbs/%(title)s|%(id)s.%(ext)s" -- "$SONG"
 
-    oldFile=$(ls "$MUSIC/thumbs" | grep "$(echo "$1" | cut -d '=' -f 2)") 
+    oldFile=$(ls "$MUSIC/thumbs" | grep -- "$(echo "$1" | cut -d '=' -f 2)") 
+    
+    mask="${oldFile%.*}-MASK.png"
     newFile="${oldFile%.*}.png"
 
-    convert "$MUSIC/thumbs/$oldFile" "$MUSIC/thumbs/$newFile"
-    rm "${MUSIC}/thumbs/${oldFile}"
+    WIDTH=100;
+
+    W=$(identify -format "%[w]" "$MUSIC/thumbs/$oldFile")
+    H=$(identify -format "%[h]" "$MUSIC/thumbs/$oldFile")
+
+    offsetX=0
+    offsetY=0
+    smallerDim=0
+
+    if [ "$W" -lt "$H" ]; then
+        offsetY=$(( ( $H - $W ) / 2))
+        smallerDim=$W
+    else
+        offsetX=$(( ( $W - $H ) / 2))
+        smallerDim=$H
+    fi
+
+    echo $W $H $offsetX $offsetY
+
+    # can prob consolidate the cropping
+    convert -size "$W"x"$H" xc:none -draw "roundrectangle $offsetX,$offsetY,$(($smallerDim + $offsetX)),$(($smallerDim + $offsetY)),$(echo "0.05 * $smallerDim" | bc),$(echo "0.08 * $smallerDim" | bc)" "$MUSIC/thumbs/$mask"
+    convert "$MUSIC/thumbs/$oldFile" -matte "$MUSIC/thumbs/$mask" -compose DstIn -composite "$MUSIC/thumbs/$newFile"
+    convert "$MUSIC/thumbs/$newFile" -crop "$smallerDim"x"$smallerDim"+"$offsetX"+"$offsetY" "$MUSIC/thumbs/$newFile"
+    
+    #703x470+3+5
+    rm "${MUSIC}/thumbs/${oldFile}" "$MUSIC/thumbs/$mask"
 fi
 
 
