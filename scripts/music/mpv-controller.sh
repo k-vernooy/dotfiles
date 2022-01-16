e150d6a203bc1157074e9921729b4a9067b83e52
k-vernooy <kai.a.vernooy@gmail.com>
2022-01-16 14:54:13 -0500

    Setup progress bars in music notifier

--- scripts/music/mpv-controller.sh
+++ scripts/music/mpv-controller.sh
@@ -7,7 +7,15 @@
 # Depends on socat and bc.
 cmd=$1
 socket=/tmp/mpv-socket
+
+notifier="dunstify"
+
 notifyText=""
+img="gnome-music"
+
+musicDir="/home/kai/music"
+thumbs="/thumbs"
+
 
 # Note: sources bash profile because this program
 #       relies on the 'mu' function, which plays
@@ -30,28 +38,47 @@ getTitle() {
         echo "waiting" > /dev/null
     done
     
-    title=$(getProp 'media-title' | tr -d '"');
-    if [ "${#title}" -gt 35 ]; then
-        title="$(echo "$title" | cut -c 1-35)..."
+    title=$(getProp 'media-title' | tr -d '"' | rev | cut -d '|' -f2- | rev);
+    if [ "${#title}" -gt 50 ]; then
+        title="$(echo "$title" | cut -c 1-50)..."
     fi
     echo "$title"
+}
+
+getTitlePlain() {
+    while [ "$(getProp seeking | tr -d '\n')" == "null" ]; do
+        echo "waiting" > /dev/null
+    done
+    
+    echo "$(getProp 'media-title' | tr -d '"' | rev | cut -d '|' -f2- | rev)"
+}
+
+
+setupNotif() {
+    notifyText="$(getTitle | iconv -c -t UTF-8)"
+    img=$(find "${musicDir}${thumbs}/" -maxdepth 1 -name "*$(printf '%q\n' "$(getTitlePlain)" )*" -exec realpath {} \;)
+    progress="-h int:value:$(getProp 'percent-pos')"
 }
 
 
 # Apply different commands based on shell args
 if [ "$cmd" = "skip" ]; then
     sendCmd "seek $2"
+    setupNotif
 elif [ "$cmd" = "skipper" ]; then
     sendCmd "seek $2 relative-percent"
+    setupNotif
 elif [ "$cmd" = "prev" ]; then
     sendCmd "playlist-prev"
 elif [ "$cmd" = "next" ]; then
     sendCmd "playlist-next"
 elif [ "$cmd" = "pause" ]; then
     sendCmd "cycle pause"
+    setupNotif
 elif [ "$cmd" = "stop" ]; then
     pkill -f "mpv --input-ipc-server"
     notifyText="Music process killed"
+    progress=""
 elif [ "$cmd" = "start" ]; then
     pkill -f "mpv --input-ipc-server"
     if [ "$2" = "shuffle" ]; then
@@ -63,10 +90,12 @@ elif [ "$cmd" = "start" ]; then
 elif [ "$cmd" = "vol" ]; then
     sendCmd "add volume $2"
 elif [ "$cmd" = "info" ]; then
-    notifyText="$(getTitle)"
+    setupNotif
 fi
+
+echo "$notifyText" | grep -axv '.*'
 
 
 if [ "$notifyText" != "" ]; then
-    notify-send "$notifyText" -a "Music" -u low -i gnome-music
+    $notifier "$notifyText" $progress -h string:x-dunst-stack-tag:music -a "Music" -u low -i "$img"
 fi
