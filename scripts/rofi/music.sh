88cef0e4d8018b5d25c2ca7d1c5cba2b7f22b75b
k-vernooy <kai.a.vernooy@gmail.com>
2022-01-16 15:03:16 -0500

    Fix partial filename launching issue; add directory and fallback icons; add directory browsing

--- scripts/rofi/music.sh
+++ scripts/rofi/music.sh
@@ -5,9 +5,16 @@ MUSIC="$HOME/music"
 
 function get_music() {
     IFS=$'\n'
-    for i in $(ls "$MUSIC" | grep -v 'thumbs'); do
+    for i in $(ls "$MUSIC/$1" | grep -v 'thumbs' | shuf); do
         name="${i%.*}"
-        echo -en "${i}\0icon\x1f${MUSIC}/thumbs/${name}.png\n"
+        
+        if [ -d "$MUSIC/$i" ]; then
+            echo -en "${i%%|*}\0icon\x1ffolder-music\n"    
+        elif [ -f "${MUSIC}/thumbs/${name}.png" ]; then
+            echo -en "${i%%|*}\0icon\x1f${MUSIC}/thumbs/${name}.png\n"
+        else
+            echo -en "${i%%|*}\0icon\x1faudio-midi\n"
+        fi
     done
 }
 
@@ -18,7 +25,11 @@ if [ -z "$@" ]; then
 else
     if [ "$1" = "Shuffle" ]; then
         $HOME/scripts/music/mpv-controller.sh start shuffle &
+    elif [ -d "$MUSIC/$1" ]; then
+        echo -en "Shuffle\0icon\x1fmedia-playlist-shuffle\n"
+        get_music "$1"
     else
-        $HOME/scripts/music/mpv-controller.sh start "${MUSIC}/$1" &
+        file="$(find music/ -maxdepth 1 -name "$(printf '%q\n' "$1")*")"
+        $HOME/scripts/music/mpv-controller.sh start "$file" &
     fi
 fi
