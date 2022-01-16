2cf2f7a9fce5d4ab0a690c28b1cabc8bd20752ea
k-vernooy <kai.a.vernooy@gmail.com>
2022-01-16 15:01:57 -0500

    Add launcher for tabmenu

--- scripts/rofi/launch.sh
+++ scripts/rofi/launch.sh
@@ -8,5 +8,7 @@ elif [ "$menu" = "appmenu" ]; then
     rofi -show drun -theme clean
 elif [ "$menu" = "powermenu" ]; then
 #    rofi -modi 'Powermenu:~/scripts/rofi/powermenu.sh' -show Powermenu -theme powermenu
-    rofi -modi 'Powermenu:~/scripts/rofi/powermenu.sh' -show Powermenu -theme powermenu -location 3 -xoffset -24 -yoffset 70
+    rofi -modi 'Powermenu:~/scripts/rofi/powermenu.sh' -show Powermenu -theme powermenu -location 3 -xoffset -30 -yoffset 86
+elif [ "$menu" = "tabmenu" ]; then
+    ~/scripts/rofi/tabmenu.sh
 fi
