06045ef8978f031bb18fa3424b0122218be8afb0
k-vernooy <kai.a.vernooy@gmail.com>
2022-07-09 21:30:34 -0400

    Update mpv  config, sort scripts

---
+++ config/mpv/scripts-manual/notify.lua
@@ -0,0 +1,22 @@
+-- based on https://github.com/rohieb/mpv-notify
+-- https://unix.stackexchange.com/a/455198/119298
+
+function do_notify()
+   local command = "bash /home/kai/scripts/music/mpv-controller.sh notif"
+   os.execute(command)
+end
+
+--function add_play()
+--   local time_played = mp.get_property('playback-time')
+--   local time_played_percent = mp.get_property('percent-pos')
+--   
+--   if (tonumber(time_played) > 300 or tonumber(time_played_percent) > 70) then
+--      os.execute("echo $(date) $(sha1sum '" .. mp.get_property('path') .. "' | cut -d ' ' -f1) >> /home/kai/scripts/music/plays.txt")
+--   end 
+--end
+
+
+-- mp.add_hook('on_load', 50, do_notify)
+
+-- mp.add_hook('on_unload', 50, add_play)
+mp.register_event("file-loaded", do_notify)
