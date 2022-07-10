953fc9acab8699cba9af477b6ab4184fe0e589cd
k-vernooy <kai.a.vernooy@gmail.com>
2022-07-10 19:01:19 -0400

    Add filebrowser to launch script and add (unused) updates to music rofi

--- scripts/rofi/launch.sh
+++ scripts/rofi/launch.sh
@@ -4,11 +4,15 @@ menu="$1"
 
 if [ "$menu" = "music" ]; then
     rofi -modi 'Music:~/scripts/rofi/music.sh' -show Music -theme music-launcher
+elif [ "$menu" = "filebrowser" ]; then
+    rofi -modi "Filebrowser:~/scripts/rofi/filebrowser.sh $2" -show Filebrowser -theme music-launcher
+    
 elif [ "$menu" = "appmenu" ]; then
     rofi -show drun -theme clean
 elif [ "$menu" = "powermenu" ]; then
 #    rofi -modi 'Powermenu:~/scripts/rofi/powermenu.sh' -show Powermenu -theme powermenu
-    rofi -modi 'Powermenu:~/scripts/rofi/powermenu.sh' -show Powermenu -theme powermenu -location 3 -xoffset -30 -yoffset 86
+    rofi -modi 'Powermenu:~/scripts/rofi/powermenu.sh' -show Powermenu -theme powermenu -location 3 -xoffset -30 -yoffset 100
+    
 elif [ "$menu" = "tabmenu" ]; then
     ~/scripts/rofi/tabmenu.sh
 fi
--- scripts/rofi/music.sh
+++ scripts/rofi/music.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 MUSIC="$HOME/music"
+SUBDIR_STATUS="$HOME/scripts/rofi/music-subdir.txt"
+SUBDIR=""
+
+
+get_subdir() {
+    SUBDIR=$(cat "$SUBDIR_STATUS" | tr -d '\n')
+}
 
 
 function get_music() {
     IFS=$'\n'
-    for i in $(ls "$MUSIC/$1" | grep -v 'thumbs' | shuf); do
+    get_subdir
+
+    for i in $(ls "$MUSIC/$SUBDIR" | grep -v 'thumbs' | shuf); do
         name="${i%.*}"
         
-        if [ -d "$MUSIC/$i" ]; then
+        if [ -d "$MUSIC/$SUBDIR/$i" ]; then
             echo -en "${i%%|*}\0icon\x1ffolder-music\n"    
         elif [ -f "${MUSIC}/thumbs/${name}.png" ]; then
             echo -en "${i%%|*}\0icon\x1f${MUSIC}/thumbs/${name}.png\n"
@@ -19,17 +28,40 @@ function get_music() {
 }
 
 
+
 if [ -z "$@" ]; then
+    echo "" > $SUBDIR_STATUS
     echo -en "Shuffle\0icon\x1fmedia-playlist-shuffle\n"
     get_music
 else
-    if [ "$1" = "Shuffle" ]; then
-        $HOME/scripts/music/mpv-controller.sh start shuffle &
-    elif [ -d "$MUSIC/$1" ]; then
-        echo -en "Shuffle\0icon\x1fmedia-playlist-shuffle\n"
-        get_music "$1"
+    get_subdir
+    if [[ "$1" == *"Shuffle"* ]]; then
+        shufArg=""
+        if [[ "$1" == *":"* ]]; then
+            shufArg="$(echo "$1" | cut -d ":" -f2)"
+        fi
+        $HOME/scripts/music/mpv-controller.sh start shuffle "$MUSIC/$shufArg" &
+        echo "" > $SUBDIR_STATUS
+    elif [[ "$1" == "Back" ]]; then
+        SUBDIR="$(echo "$SUBDIR" | rev | cut -d '/' -f 2- | rev)"
+        echo "$SUBDIR" > $SUBDIR_STATUS
+    
+        echo -en "Shuffle:$SUBDIR\0icon\x1fmedia-playlist-shuffle\n"
+        
+        if [ "$SUBDIR" != "" ]; then
+            echo -en "Back\0icon\x1farrow-left\n"
+        fi
+        get_music
+     elif [ -d "$MUSIC/$SUBDIR/$1" ]; then
+        echo "$SUBDIR/$1" > $SUBDIR_STATUS
+
+        get_subdir
+        echo -en "Shuffle:$SUBDIR\0icon\x1fmedia-playlist-shuffle\n"
+        echo -en "Back\0icon\x1farrow-left\n"
+        get_music
     else
-        file="$(find music/ -maxdepth 1 -name "$(printf '%q\n' "$1")*")"
-        $HOME/scripts/music/mpv-controller.sh start "$file" &
+        file="$HOME/$(find "music/$SUBDIR" -name "$(printf '%q\n' "$1")*" -not -path "music/thumbs/*")"
+        $HOME/scripts/music/mpv-controller.sh start "$file" "" &
+        echo "" > $SUBDIR_STATUS
     fi
 fi
