10dbf6d185e295057dc13cfa60207ab14064eca6
k-vernooy <kai.a.vernooy@gmail.com>
2022-07-10 19:04:56 -0400

    Update music controller with metadata retriever

---
+++ scripts/music/mpv-controller-vanilla.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+shopt -s extglob
+
+# This script is used to control an mpv session
+# connected to $socket. Can send signals
+# to pause/play, rewind/fast-forward, advance/go back,
+# and to restart the music process.
+# Depends on socat and bc.
+cmd=$1
+socket=/tmp/mpv-socket
+
+notifier="dunstify"
+
+notifyText=""
+img="gnome-music"
+
+musicDir="/home/kai/music"
+thumbs="/thumbs"
+
+# . ~/.bash_profile > /dev/null 2>&1
+
+
+# Add mpv socket command sender - takes property string
+getProp() {
+    echo "{ \"command\": [\"get_property\", \""$1"\"] }" | socat - $socket | jq .data
+}
+
+sendCmd() {
+    echo "$1" | socat - "$socket"
+}
+
+# Get the truncated title
+getTitle() {
+    while [ "$(getProp seeking | tr -d '\n')" == "null" ]; do
+        echo "waiting" > /dev/null
+    done
+    
+    title=$(getProp 'media-title' | tr -d '"' | rev | cut -d '|' -f2- | rev);
+    if [ "${#title}" -gt 50 ]; then
+        title="$(echo "$title" | cut -c 1-50)..."
+    fi
+    echo "$title"
+}
+
+getTitlePlain() {
+    while [ "$(getProp seeking | tr -d '\n')" == "null" ]; do
+        echo "waiting" > /dev/null
+    done
+    
+    echo "$(getProp 'media-title' | tr -d '"')"
+}
+
+
+setupNotif() {
+    notifyText="$(getTitle | iconv -c -t UTF-8)"
+
+    title=$(getTitlePlain)
+    secondHalf=$(echo "$title" | rev | cut -d '|' -f 1 | cut -d '.' -f2- | rev)
+
+    echo "$title"
+    find "${musicDir}${thumbs}/" -maxdepth 1 -name "*$(printf '%q\n' "$(echo "$title" | cut -d '|' -f1)" )*" -exec realpath {} \;
+
+    img=$(find "${musicDir}${thumbs}/" -maxdepth 1 -name "*$(printf '%q\n' "$(echo "$title" | cut -d '|' -f1)" )*" -exec realpath {} \; | grep "$secondHalf" | grep '.png$')  
+    progress="-h int:value:$(getProp 'percent-pos')"
+}
+
+
+# Apply different commands based on shell args
+if [ "$cmd" = "skip" ]; then
+    sendCmd "seek $2"
+    setupNotif
+elif [ "$cmd" = "skipper" ]; then
+    sendCmd "seek $2 relative-percent"
+    setupNotif
+elif [ "$cmd" = "prev" ]; then
+    sendCmd "playlist-prev"
+elif [ "$cmd" = "next" ]; then
+    sendCmd "playlist-next"
+elif [ "$cmd" = "pause" ]; then
+    sendCmd "cycle pause"
+    setupNotif
+elif [ "$cmd" = "stop" ]; then
+    pkill -f "mpv --script=~/.config/mpv/scripts-manual/notify.lua"
+    notifyText="Music process killed"
+    progress=""
+elif [ "$cmd" = "start" ]; then
+    pkill -f "mpv --script=~/.config/mpv/scripts-manual/notify.lua"
+    
+    if [ "$2" = "shuffle" ]; then
+        #playDir=~/music/!(*misc/)
+        #if [ "$3" != "" ]; then
+        #    playDir=~/music/$3/*
+        #fi
+        playDir="$3"
+        mpv --script=~/.config/mpv/scripts-manual/notify.lua --input-ipc-server="${socket}" -- $playDir > /dev/null 2>&1 &
+    else
+        mpv --script=~/.config/mpv/scripts-manual/notify.lua --input-ipc-server="${socket}" -- "$2"  > /dev/null 2>&1 &
+    fi
+    
+    # ...why is this here??
+    # sleep 0.5
+elif [ "$cmd" = "vol" ]; then
+    sendCmd "add volume $2"
+elif [ "$cmd" = "info" ]; then
+    setupNotif
+elif [ "$cmd" = "infotext" ]; then
+    getTitle | iconv -c -t UTF-8
+fi
+
+echo "$notifyText" | grep -axv '.*'
+
+
+if [ "$notifyText" != "" ]; then
+    $notifier "$notifyText" $progress -h string:x-dunst-stack-tag:music -a "Composer" -u low -i "$img"
+fi
--- scripts/music/mpv-controller.sh
+++ scripts/music/mpv-controller.sh
@@ -1,101 +1,164 @@
 #!/bin/bash
 
+shopt -s extglob
+
 # This script is used to control an mpv session
 # connected to $socket. Can send signals
 # to pause/play, rewind/fast-forward, advance/go back,
 # and to restart the music process.
 # Depends on socat and bc.
 cmd=$1
-socket=/tmp/mpv-socket
 
-notifier="dunstify"
+SOCKET=/tmp/mpv-socket
+VENV="$HOME/venv"
+MUSICDIR="$HOME/music"
+NOTIFIER="dunstify"
+TAGSCRIPT="$MUSICDIR/metadata/tag.py"
 
 notifyText=""
+notifyTitle=""
 img="gnome-music"
 
-musicDir="/home/kai/music"
 thumbs="/thumbs"
 
-
-# Note: sources bash profile because this program
-#       relies on the 'mu' function, which plays
-#       my music directory on shuffle.
-. ~/.bash_profile > /dev/null 2>&1
-
+. "$VENV/bin/activate"
 
 # Add mpv socket command sender - takes property string
-getProp() {
-    echo "{ \"command\": [\"get_property\", \""$1"\"] }" | socat - $socket | jq .data
+getMpvProp() {
+    echo "{ \"command\": [\"get_property\", \""$1"\"] }" | socat - $SOCKET | jq .data
 }
 
-sendCmd() {
-    echo "$1" | socat - "$socket"
+getMpvPath() {
+    getMpvProp "path" | sed 's/\/\//\//g' | cut -c 2- | rev | cut -c 2- | rev
 }
+
+sendMpvCmd() {
+    echo "$1" | socat - "$SOCKET"
+}
+
+# TITLE=""
+# ARTIST=""
+# COMPOSER=""
+
+getMetadataProp() {
+    # echo "$(getMpvPath)"
+    python3 "$TAGSCRIPT" get "$1" "$(getMpvPath)" #| fold -s -w 20 | python3 ~/scripts/misc/center-multiline.py
+    # TITLE="$(echo "$tags" | grep "TITLE" | cut -d ':' -f 2-)"
+    # ARTIST="$(echo "$tags" | grep "ARTIST" | cut -d ':' -f 2-)"
+    # COMPOSER="$(echo "$tags" | grep "COMPOSER" | cut -d ':' -f 2-)"
+}
+
 
 # Get the truncated title
 getTitle() {
-    while [ "$(getProp seeking | tr -d '\n')" == "null" ]; do
+    while [ "$(getMpvProp seeking | tr -d '\n')" == "null" ]; do
         echo "waiting" > /dev/null
     done
     
-    title=$(getProp 'media-title' | tr -d '"' | rev | cut -d '|' -f2- | rev);
+    title=$(getMpvProp 'media-title' | tr -d '"' | rev | cut -d '|' -f2- | rev);
     if [ "${#title}" -gt 50 ]; then
         title="$(echo "$title" | cut -c 1-50)..."
     fi
     echo "$title"
 }
 
+
 getTitlePlain() {
-    while [ "$(getProp seeking | tr -d '\n')" == "null" ]; do
+    while [ "$(getMpvProp seeking | tr -d '\n')" == "null" ]; do
         echo "waiting" > /dev/null
     done
     
-    echo "$(getProp 'media-title' | tr -d '"' | rev | cut -d '|' -f2- | rev)"
+    echo "$(getMpvProp 'media-title' | tr -d '"')"
+}
+
+
+getThumb() {
+    title=$(getTitlePlain)
+
+    if [ "$title" != "" ]; then
+        secondHalf=$(echo "$title" | rev | cut -d '|' -f 1 | cut -d '.' -f2- | rev)
+        find "${MUSICDIR}${thumbs}/" -maxdepth 1 -name "*$(printf '%q\n' "$(echo "$title" | cut -d '|' -f1)" )*" -exec realpath {} \;
+    fi
 }
 
 
 setupNotif() {
-    notifyText="$(getTitle | iconv -c -t UTF-8)"
-    img=$(find "${musicDir}${thumbs}/" -maxdepth 1 -name "*$(printf '%q\n' "$(getTitlePlain)" )*" -exec realpath {} \;)
-    progress="-h int:value:$(getProp 'percent-pos')"
+    # notifyText="$(getTitle | iconv -c -t UTF-8)"
+    if [ "$(getMetadataProp "")" = "" ]; then
+        notifyTitle="Music"
+        notifyText="$(getTitle | iconv -c -t UTF-8)"
+    else
+        notifyTitle="$(getMetadataProp COMPOSER)"
+        notifyText="$(getMetadataProp TITLE) - $(getMetadataProp ARTISTS)"
+    fi
+    title=$(getTitlePlain)
+    secondHalf=$(echo "$title" | rev | cut -d '|' -f 1 | cut -d '.' -f2- | rev)
+
+    # find "${MUSICDIR}${thumbs}/" -maxdepth 1 -name "*$(printf '%q\n' "$(echo "$title" | cut -d '|' -f1)" )*" -exec realpath {} \;
+
+    img=$(find "${MUSICDIR}${thumbs}/" -maxdepth 1 -name "*$(printf '%q\n' "$(echo "$title" | cut -d '|' -f1)" )*" -exec realpath {} \; | grep "$secondHalf" | grep '.png$')  
+    progress="-h int:value:$(getMpvProp 'percent-pos')"
 }
 
 
 # Apply different commands based on shell args
-if [ "$cmd" = "skip" ]; then
-    sendCmd "seek $2"
+if [ "$cmd" = "seek" ]; then
+    sendMpvCmd "seek $2"
     setupNotif
-elif [ "$cmd" = "skipper" ]; then
-    sendCmd "seek $2 relative-percent"
+elif [ "$cmd" = "seek-percent" ]; then
+    sendMpvCmd "seek $2 relative-percent"
     setupNotif
+elif [ "$cmd" = "seek-chapter" ]; then
+    exit
 elif [ "$cmd" = "prev" ]; then
-    sendCmd "playlist-prev"
+    sendMpvCmd "playlist-prev"
 elif [ "$cmd" = "next" ]; then
-    sendCmd "playlist-next"
+    sendMpvCmd "playlist-next"
 elif [ "$cmd" = "pause" ]; then
-    sendCmd "cycle pause"
+    sendMpvCmd "cycle pause"
     setupNotif
 elif [ "$cmd" = "stop" ]; then
-    pkill -f "mpv --input-ipc-server"
-    notifyText="Music process killed"
+    pkill -f "mpv --script=~/.config/mpv/scripts-manual/notify.lua"
+    notifyTitle="Music"
+    notifyText="MPV process killed"
     progress=""
 elif [ "$cmd" = "start" ]; then
-    pkill -f "mpv --input-ipc-server"
+    #pkill -f "mpv --script=~/.config/mpv/scripts-manual/notify.lua"
+    
     if [ "$2" = "shuffle" ]; then
-        mu > /dev/null 2>&1 &
+        #playDir=~/music/!(*misc/)
+        #if [ "$3" != "" ]; then
+        #    playDir=~/music/$3/*
+        #fi
+        playDir="$3"
+        mpv --script=~/.config/mpv/scripts-manual/notify.lua --input-ipc-server="${SOCKET}" --shuffle -- "$playDir" > /dev/null 2>&1 &
     else
-        mpv --input-ipc-server="${socket}" -- "${2}"  > /dev/null 2>&1 &
+        echo "$2" > ~/toasters.txt
+        mpv --script=~/.config/mpv/scripts-manual/notify.lua --input-ipc-server="${SOCKET}" -- "$2"  > /dev/null 2>&1 &
     fi
-    sleep 0.5
+    
+    # sleep 0.5
 elif [ "$cmd" = "vol" ]; then
-    sendCmd "add volume $2"
-elif [ "$cmd" = "info" ]; then
+    sendMpvCmd "add volume $2"
+elif [ "$cmd" = "notif" ]; then
     setupNotif
+elif [ "$cmd" = "mpvtitle" ]; then
+    getTitle | iconv -c -t UTF-8
+elif [ "$cmd" = "get" ]; then
+    getMetadataProp "$2"
+elif [ "$cmd" = "getmpv" ]; then
+    getMpvProp "$2"
+elif [ "$cmd" = "thumb" ]; then
+    getThumb
+elif [ "$cmd" = "title" ]; then
+    getTitle
 fi
+
 
 echo "$notifyText" | grep -axv '.*'
 
 
 if [ "$notifyText" != "" ]; then
-    $notifier "$notifyText" $progress -h string:x-dunst-stack-tag:music -a "Music" -u low -i "$img"
+#    "$NOTIFIER" "$notifyText" $progress -h string:x-dunst-stack-tag:music -a "$(getMetadataProp COMPOSER)" -u low -i "$img"
+    "$NOTIFIER" -a "$notifyTitle" "$notifyText" $progress -r 98294 -u low -i "$img"
 fi
